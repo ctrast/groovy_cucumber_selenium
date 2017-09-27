201d5bc80ef16002db95f21d9f09cc028a7d30a2
Cory Trast <cory_trast@hotmail.com>
2017-09-27 12:53:25 -0500

    updates

--- CurrentSprint.bat
+++ CurrentSprint.bat
@@ -1,4 +1,6 @@
-cd C:\Workspace\projectname
+set projectLocation=C:\Dev\Groovy_Cucumber_Selenium
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java -cp C:\Workspace\projectname\lib*;C:\Workspace\projectname\bin org.testng.TestNG currentsprint.xml
 pause
 
