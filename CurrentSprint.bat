503ede0be71f0e621dd02d6c77183fba1e7182a1
Cory Trast <cory_trast@hotmail.com>
2017-09-27 14:06:49 -0500

    updates

--- CurrentSprint.bat
+++ CurrentSprint.bat
@@ -1,4 +1,4 @@
 set projectLocation=C:\Dev\Groovy_Cucumber_Selenium
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java -cp "$USER_HOME$/.gradle/caches/modules-2/files-2.1/org.testng/testng/6.9.10/6feb3e964aeb7097aff30c372aac3ec0f8d87ede/testng-6.9.10.jar" org.testng.TestNG  currentsprint.xml
+java -cp "C:\Users\C\.gradle\caches\modules-2\files-2.1\org.testng\testng\6.9.10\6feb3e964aeb7097aff30c372aac3ec0f8d87ede\testng-6.9.10.jar" org.testng.TestNG  currentsprint.xml
