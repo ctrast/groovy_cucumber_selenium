112029514b8b5a2e8ff3108ef4185f56fc1a5bf7
Cory Trast <cory_trast@hotmail.com>
2017-09-27 15:34:01 -0500

    updates

--- CurrentSprint.bat
+++ CurrentSprint.bat
@@ -1,5 +1,3 @@
 set projectLocation=C:\Dev\Groovy_Cucumber_Selenium
 cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-
-java -cp "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2017.2.4\plugins\testng\lib\" org.testng.TestNG currentsprint.xml
+java -cp "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2017.2.4\plugins\testng\lib:C:\Dev\Groovy_Cucumber_Selenium\src\test\groovy\support\runners" org.testng.TestNG testng.xml
